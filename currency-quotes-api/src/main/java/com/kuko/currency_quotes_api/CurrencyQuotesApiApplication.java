cc03deff8f5ea0ad85726b7353643ca08f3b7ab8
Tobechukwu Ofili <ofilitobe@gmail.com>
2025-09-08 22:04:45 +0100

    Scaffold Spring Boot Project

---
+++ currency-quotes-api/src/main/java/com/kuko/currency_quotes_api/CurrencyQuotesApiApplication.java
@@ -0,0 +1,13 @@
+package com.kuko.currency_quotes_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyQuotesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyQuotesApiApplication.class, args);
+	}
+
+}
